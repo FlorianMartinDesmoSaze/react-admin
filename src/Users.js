888d4d31c9ea2071085e4b4e9f5cf5b696cf066d
florian <martinflorian2@gmail.com>
2022-10-21 11:06:29 +0200

    custom field wbesite

--- src/Users.js
+++ src/Users.js
@@ -1,16 +1,18 @@
 import * as React from "react";
-import { Datagrid, EmailField, List, TextField } from 'react-admin';
+import { Datagrid, EmailField, List, TextField, UrlField } from 'react-admin';
+import MyUrlField from "./MyUrlField";
 
 export const UserList = () => (
     <List>
         <Datagrid rowClick="edit">
             <TextField source="id" />
             <TextField source="name" />
-            <TextField source="username" />
+            {/* <TextField source="username" /> */}
             <EmailField source="email" />
-            <TextField source="address.street" />
+            {/* <TextField source="address.street" /> */}
             <TextField source="phone" />
-            <TextField source="website" />
+            {/* <UrlField source="website" /> */}
+            <MyUrlField source="website" />
             <TextField source="company.name" />
         </Datagrid>
     </List>
