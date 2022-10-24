f37f08ecb4ec943233c1f0c8713ebff8b39d2a7e
florian <martinflorian2@gmail.com>
2022-10-24 12:04:49 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Admin, Resource/*, ListGuesser*/ } from 'react-admin';
+import { Admin, Resource/*, ListGuesser*/, EditGuesser } from 'react-admin';
 import { PostList } from './posts'
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from "./users";
@@ -9,7 +9,7 @@ const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dataProvider={dataProvider}>
     {/* <Resource name="posts" list={ListGuesser} /> */}
-    <Resource name="posts" list={PostList} />
+    <Resource name="posts" list={PostList} edit={EditGuesser} />
     <Resource name="users" list={UserList} recordRepresentation="name" />
   </Admin>
 );
