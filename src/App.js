39d03a20ecb354e946b9463aa7edaffa15cec5a5
florian <martinflorian2@gmail.com>
2022-10-25 11:10:13 +0200

    dashboard

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from "./users";
 import PostIcon from '@mui/icons-material/Book';
 import UserIcon from '@mui/icons-material/Group';
+import Dashboard from './Dashboard'
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
     {/* <Resource name="posts" list={ListGuesser} /> */}
     {/* <Resource name="edit" list={PostEdit} edit={EditGuesser} /> */}
     {/* <Resource name="posts" list={PostList} edit={EditGuesser} /> */}
---
+++ src/Dashboard.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { Card, CardContent, CardHeader } from '@mui/material';
+
+const Dashboard = () => (
+    <Card>
+        <CardHeader title="Administration page" />
+        <CardContent>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed, quam id minus explicabo ullam adipisci odio vitae officia est excepturi delectus maxime maiores eaque ex dolor. Autem animi quia sequi?</CardContent>
+    </Card>
+)
+
+export default Dashboard;
